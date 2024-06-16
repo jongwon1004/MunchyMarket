42ab2a250b9e019d21a73de8a03102914e211987
jongwon1004 <whddnjs3340@naver.com>
2024-06-16 09:14:48 +0900

    update : Entity化メソッド追加

--- src/main/java/com/munchymarket/MunchyMarket/domain/Member.java
+++ src/main/java/com/munchymarket/MunchyMarket/domain/Member.java
@@ -6,24 +6,42 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@ToString(of = {"id", "loginId", "name", "ruby", "email", "phoneNumber", "sex", "birth", "role"})
 public class Member extends TimeBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false, updatable = false)
     private Long id;
+
 
     @Column(nullable = false, length = 30, unique = true)
     private String loginId;
 
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String ruby;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false)
+    private String sex;
+
+    @Column(nullable = false)
     private String birth;
 
     @Enumerated(EnumType.STRING)
@@ -38,13 +56,14 @@ public class Member extends TimeBaseEntity {
 
 
     @Builder
-    public Member(String loginId, String password, String name, String ruby , String email, String phoneNumber, String birth, String role) {
+    public Member(String loginId, String password, String name, String ruby, String email, String phoneNumber, String sex, String birth, String role) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.ruby = ruby;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.sex = sex;
         this.birth = birth;
         this.role = Role.fromKey(role);
     }
