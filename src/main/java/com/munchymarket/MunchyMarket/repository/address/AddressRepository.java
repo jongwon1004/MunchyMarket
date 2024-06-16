295bc0866b5ea547b783c0ef134a984aab80f1d6
jongwon1004 <whddnjs3340@naver.com>
2024-06-16 09:19:01 +0900

    add : AddressRepository 追加

---
+++ src/main/java/com/munchymarket/MunchyMarket/repository/address/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.munchymarket.MunchyMarket.repository.address;
+
+import com.munchymarket.MunchyMarket.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
