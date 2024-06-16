92e8601de2d63737167e453a38a76a9a9433ff76
jongwon1004 <whddnjs3340@naver.com>
2024-06-16 09:13:57 +0900

    update : String -> LocalDateTime ミスったので、変更

--- src/main/java/com/munchymarket/MunchyMarket/domain/base/TimeBaseEntity.java
+++ src/main/java/com/munchymarket/MunchyMarket/domain/base/TimeBaseEntity.java
@@ -7,15 +7,17 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class TimeBaseEntity {
 
     @CreatedDate
     @Column(name = "created_date")
-    private String createdDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     @Column(name = "last_modified_date")
-    private String lastModifiedDate;
+    private LocalDateTime lastModifiedDate;
 }
