5ba53d558f417520a60d16c22cecd55326d5c2d9
jongwon1004 <whddnjs3340@naver.com>
2024-06-16 09:17:47 +0900

    add : ExceptionHandler 追加

---
+++ src/main/java/com/munchymarket/MunchyMarket/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.munchymarket.MunchyMarket.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
---
+++ src/main/java/com/munchymarket/MunchyMarket/exception/JoinExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.munchymarket.MunchyMarket.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice(basePackageClasses = com.munchymarket.MunchyMarket.controller.MemberController.class)
+public class JoinExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        log.info("Validation error: {}", errors);
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
