fdc306cd767d37872b21753ac84a4952bf546d4f
jongwon1004 <whddnjs3340@naver.com>
2024-06-24 07:55:44 +0900

    commit

--- src/test/java/com/munchymarket/MunchyMarket/MunchyMarketApplicationTests.java
+++ src/test/java/com/munchymarket/MunchyMarket/MunchyMarketApplicationTests.java
@@ -2,8 +2,10 @@ package com.munchymarket.MunchyMarket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+//@Sql(scripts = {"classpath:data.sql"})
 class MunchyMarketApplicationTests {
 
 	@Test
