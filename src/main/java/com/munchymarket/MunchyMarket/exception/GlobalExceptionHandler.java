3e46a9f0df22718a0f7eaf6f753e7f4f4814db4e
jongwon1004 <whddnjs3340@naver.com>
2024-06-24 07:55:33 +0900

    add : IllegalArgumentException追加

--- src/main/java/com/munchymarket/MunchyMarket/exception/GlobalExceptionHandler.java
+++ src/main/java/com/munchymarket/MunchyMarket/exception/GlobalExceptionHandler.java
@@ -1,7 +1,36 @@
 package com.munchymarket.MunchyMarket.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class})
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(Exception ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        if (ex instanceof MethodArgumentNotValidException validException) {
+            validException.getBindingResult().getAllErrors().forEach((error) -> {
+                String fieldName = ((FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            });
+            log.info("Validation error: {}", errors);
+        } else if (ex instanceof IllegalArgumentException) {
+            errors.put("error", ex.getMessage());
+            log.info("Illegal argument error: {}", ex.getMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
