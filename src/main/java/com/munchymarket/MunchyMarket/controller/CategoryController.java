940e88fd449272b1d1f29c478ccf6dd3968f3b46
jongwon1004 <whddnjs3340@naver.com>
2024-07-04 15:21:18 +0900

    add: SortType Entityを使ったロジックに変更

--- src/main/java/com/munchymarket/MunchyMarket/controller/CategoryController.java
+++ src/main/java/com/munchymarket/MunchyMarket/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.munchymarket.MunchyMarket.controller;
 
+import com.munchymarket.MunchyMarket.domain.SortType;
 import com.munchymarket.MunchyMarket.dto.*;
+import com.munchymarket.MunchyMarket.exception.ErrorCode;
 import com.munchymarket.MunchyMarket.repository.category.CategoryRepository;
 import com.munchymarket.MunchyMarket.repository.sorttype.SortTypeRepository;
 import com.munchymarket.MunchyMarket.service.CategoryService;
@@ -65,7 +67,7 @@ public class CategoryController {
     public ResponseEntity<ProductListResponseDto> products(@PathVariable("categoryId") Long categoryId,
                                                            @RequestParam("page") int page,
                                                            @RequestParam(value = "size", defaultValue = "9") int size,
-                                                           @RequestParam(value = "sorted_type", defaultValue = "id") String sort){
+                                                           @RequestParam(value = "sorted_type", defaultValue = "1") long sortId){
 
         /**
          * sorted_type = 0, 신상품 순
@@ -75,14 +77,24 @@ public class CategoryController {
          * +--------------+---------------------+----------------+
          * | sort_type_id | sort_type_field     | sort_direction |
          * +--------------+---------------------+----------------+
-         * | 0            | createdDate        | desc           |
-         * | 1            | salesCount         | desc           |
-         * | 2            | price               | asc            |
-         * | 3            | price               | desc           |
+         * | 1            | createdDate        | desc           |
+         * | 2            | salesCount         | desc           |
+         * | 3            | basePrice          | asc            |
+         * | 4            | basePrice          | desc           |
          * +--------------+---------------------+----------------+
          */
 
-        PageRequest pageRequest = PageRequest.of(page-1, size, Sort.by(sort).descending());
+        log.info("sortId = {}", sortId);
+
+
+        SortType sortType = sortTypeRepository.findById(sortId)
+                .orElseThrow(() -> new IllegalArgumentException(ErrorCode.SORT_TYPE_NOT_FOUND.getMessage()));
+
+        Sort sort = Sort.by(sortType.getSortTypeField());
+        sort = sortType.getSortDirection()
+                .equals("desc") ? sort.descending() : sort.ascending();
+
+        PageRequest pageRequest = PageRequest.of(page-1, size, sort);
         log.info("pageRequest = {}", pageRequest);
         log.info("pageRequest.getOffset() = {}", pageRequest.getOffset());
 
@@ -90,7 +102,7 @@ public class CategoryController {
 
         Slice<ProductDto> productSlice = new SliceImpl<>(productPage.getContent(), pageRequest, productPage.hasNext());
         ProductListResponseDto productListResponseDto =
-                new ProductListResponseDto(productSlice,productPage.getTotalElements(), productPage.getTotalPages());
+                new ProductListResponseDto(sortType.getDisplayName(), productSlice, productPage.getTotalElements(), productPage.getTotalPages());
 
         return ResponseEntity.ok().body(productListResponseDto);
     }
