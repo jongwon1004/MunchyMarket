31fec05a7b0120b4c8b98e5bf9e5bedbc1649201
jongwon1004 <whddnjs3340@naver.com>
2024-12-02 14:39:37 +0900

    commit

--- src/main/java/com/munchymarket/MunchyMarket/config/SecurityConfig.java
+++ src/main/java/com/munchymarket/MunchyMarket/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
                                 "/api/member/send-sms", "/api/member/validate", "/api/member/verification-code", "/api/member/join",
                                 "/api/admin/product/register", "/api/admin/product/categories", "/api/admin/product/packaging-types", //テスト段階なので、一時的に許可
                                 "/api/categories/**",
+                                "/favicon.ico", "/robots.txt", "/sitemap.xml",
                                 "/logout", "/error", "/swagger-ui-custom.html").permitAll() // 誰でもアクセス可能。requestMatchers() に記載されたURLは認証、認可がなくてもアクセス可能
                         .requestMatchers("/api/member/role-check").hasRole("ADMIN") // ADMIN　権限を持つユーザーだけアクセス可能
                         .requestMatchers("/profile", "/api/accounts/register/", "/api/accounts/**").authenticated() // 認証済みのユーザーだけアクセス可能
