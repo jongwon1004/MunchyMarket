0ba1a24757a6f469ea87b055b27f1b871774d2cc
jongwon1004 <whddnjs3340@naver.com>
2024-12-03 22:10:35 +0900

    add: webhooks intent created

--- src/main/java/com/munchymarket/MunchyMarket/controller/pay/PaymentWebhooksController.java
+++ src/main/java/com/munchymarket/MunchyMarket/controller/pay/PaymentWebhooksController.java
@@ -6,11 +6,13 @@ import com.stripe.model.Event;
 import com.stripe.model.PaymentIntent;
 import com.stripe.net.Webhook;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/webhooks")
@@ -23,24 +25,31 @@ public class PaymentWebhooksController {
     @PostMapping("/stripe")
     public ResponseEntity<String> handleStripeWebhook(@RequestBody String payload, @RequestHeader("Stripe-Signature") String sigHeader) {
 
+
         try {
             // Stripe의 시그니처 확인
             Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
 
             // 이벤트 처리
             switch (event.getType()) {
+
+                case "payment_intent.created":
+                    PaymentIntent createdPaymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().orElse(null);
+                    log.info("PaymentIntent created = {}", createdPaymentIntent.getId());
+                    break;
+
                 case "payment_intent.succeeded":
                     PaymentIntent paymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().orElse(null);
-                    System.out.println("PaymentIntent succeeded: " + paymentIntent.getId());
+                    log.info("PaymentIntent succeeded = {}", paymentIntent.getId());
                     break;
 
                 case "charge.succeeded":
                     Charge charge = (Charge) event.getDataObjectDeserializer().getObject().orElse(null);
-                    System.out.println("Charge succeeded: " + charge.getId());
+                    log.info("Charge succeeded = {}", charge.getId());
                     break;
 
                 default:
-                    System.out.println("Unhandled event type: " + event.getType());
+                    log.info("Unhandled event type: {}", event.getType());
             }
 
             return ResponseEntity.ok("Webhook processed successfully");
